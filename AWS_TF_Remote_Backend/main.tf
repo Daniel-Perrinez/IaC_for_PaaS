50ced36d748a8d42b2a06bc91b144bf0ae5eb3bc
--unset <--unset>
2024-02-22 10:57:14 -0500

    adding terraform pipeline

--- AWS_TF_Remote_Backend/main.tf
+++ AWS_TF_Remote_Backend/main.tf
@@ -10,9 +10,9 @@ terraform {
 
 resource "aws_s3_bucket" "paas-terraform-remote-state-bucket" {
   bucket  = var.terraform_state_bucket_name
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
   tags = var.tags
 }
 
@@ -20,7 +20,9 @@ resource "aws_dynamodb_table" "tf-remote-state-lock" {
   name = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
-
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
   attribute {
     name = "LockID"
     type = "S"
