57b4f8c089b073c0347441786b73bdbc8c1d25d4
--unset <--unset>
2024-03-04 11:21:00 -0500

    attached policy to role

--- AWS_TF_Remote_Backend/iam.tf
+++ AWS_TF_Remote_Backend/iam.tf
@@ -16,3 +16,8 @@ resource "aws_iam_policy" "backend-policy" {
   name   = "backend-policy"
   policy = file("policies/backend-policy.json")
 }
+
+resource "aws_iam_role_policy_attachment" "github_actions_role_policy_attachement" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::905418106762:policy/backend-policy"
+}
